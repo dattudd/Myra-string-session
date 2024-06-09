fca89abc9f995320737a8b9026f6d4f59a7f7a5a
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-06-09 22:04:02 +0530

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -9,7 +9,7 @@ keyboard = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="sᴏᴜʀᴄᴇ", url="https://github.com/AnonymousX1025/StringGenBot"
+                text="owner", url="https://t.me/BTSChinna_op"
             ),
         ],
     ]
